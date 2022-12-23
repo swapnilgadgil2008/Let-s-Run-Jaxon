1a46e4110a8c7b922dd62177eec9346cfbef1d36
swapnilgadgil2008 <120476618+swapnilgadgil2008@users.noreply.github.com>
2022-12-22 19:48:58 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+  //creating boy running
+  boy = createSprite(200,400,50,50);
+  boy.scale=0.08;
+  boy.addAnimation("Jack",boyImg);
+
+  
+
+leftBoundary=createSprite(0,0,100,800);
+
+leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+
+
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  if(path.y > 400 ){
+  path.y = height/2;
+  }
+
+
+  
+  drawSprites();
+}
